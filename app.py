ee6cf9efd144a1c9495b41fdc4ce1e5fbd93cac2
bo <bo@nomad.local>
2025-04-29 15:08:29 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+class Telestrations:
+    def __init__(self):
+        self.client = OpenAI()
+        self.image_from_prompt_model = "gpt-3.5-turbo"
+        self.prompt_from_image_model = "gpt-4o-mini"
+        self.styles = {"realistic", "colorful"}
+        self.max_prompt_length = 100
+        self.max_prompt_words = 6
+        self.image_size = "256x256"
+
+    def set_model(self, model_string):
+        self.model = model_string
+
+    def add_style(self, style):
+        self.styles.add(style)
+
+    def remove_style(self, style):
+        self.styles.remove(style)
+
+    def get_image_url_from_prompt(self, image_prompt):
+        if len(image_prompt) > self.max_prompt_length:
+            print("prompt is too long (max length: " + str(self.max_prompt_length) + ")")
+            return None
+
+        img = self.client.images.generate(
+            prompt = "{p}, {s}".format(p=image_prompt, s=', '.join(self.styles)),
+            size= self.image_size
+        )
+        return img.data[0].url
+
+    def get_prompt_from_image(self, image_url):
+        chat = self.client.chat.completions.create(
+            model = self.prompt_from_image_model,
+            messages=[
+                {"role":"user","content":[
+                    {"type":"text","text":"Generate a 1 to {w} word prompt that might generate this picture".format(w=self.max_prompt_words)},
+                    {"type":"image_url","image_url":{"url":image_url}}
+                ]}
+            ])
+        return chat.choices[0].message.content
+
+T = Telestrations()
+img = T.get_image_url_from_prompt("Infinite piano")
+print(img)
+print()
+new_prompt = T.get_prompt_from_image(img)
+print()
+input()
+
+for i in range(3):
+    img = T.get_image_url_from_prompt(new_prompt)
+    print(new_prompt + ":\n" + img)
+    print()
+    input()
+    new_prompt = T.get_prompt_from_image(img)
